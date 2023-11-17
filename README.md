a516a641df7f0953d44be7819f4b362ec4e01f12
Will Dickson <will@iorodeo.com>
2023-11-17 09:22:14 -0800

    Worked on relative component placement. Added images.

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-flypanel-layout-tools:
+## flypanel-layout-tools
 
 Python library and command line tools for design and layout of flypanel arenas.
 
+
+
+### Installation
+
+Can be installed with pip or [poetry](https://python-poetry.org/).
+
+Using pip
+
+```bash
+pip install .
+```
+
+Using poetry
+```bash
+poetry install 
+```
+
+### Command line interface
+
+![cli_screenshot](cli_screenshot.png)
+
+
+### Example output
+
+
+cli output 
+![cli_example_output](cli_example_output.png)
+
+
+arena plot
+![arena_plot_example](8-8-ring-arena_plot.png)
+
+layout example
+![arena_plot_example](8-8_arena_layout_example.png)
--- flypanel_layout_tools/ring_layout.py
+++ flypanel_layout_tools/ring_layout.py
@@ -172,6 +172,8 @@ class RingLayout:
         ax.axis('equal')
         ax.grid(True)
         ax.set_title(title)
+        print('close figure window to continue')
+        print()
         plt.show()
 
     def place_components(self, filename):
@@ -181,32 +183,37 @@ class RingLayout:
         self.print_pcb_params(filename, pcb_params)
 
         # Extract placement parameters
-        pcb_cx = pcb_params['center_x']
-        pcb_cy = pcb_params['center_y']
         ref_prefix = pcb_params['panel']['ref_prefix']
         ref_start = pcb_params['panel']['ref_start']
-        num = self.values['num_installed']
-        panel_ref_list = [f'{ref_prefix}{i}' for i in range(ref_start, ref_start+num)]
 
-        # Load board and place components
+        # Get list of panel header references
+        num = self.values['num_installed']
+        panel_num_list = list(range(ref_start, ref_start+num))
+        panel_ref_list = [f'{ref_prefix}{i}' for i in panel_num_list]
+
         pcb = pcbnew.LoadBoard(filename)
 
-        # Get relative positions from model panel
+        # Get Dictionary mapping panel reference to the reference for all components to be
+        # placed relative to the header
+        panel_ref_to_rel = get_panel_ref_to_rel(pcb_params, panel_num_list, panel_ref_list)
 
-        for ind, panel_ref in enumerate(panel_ref_list):
-            footprint = pcb.FindFootprintByReference(panel_ref)
-            angle = self.values['angles'][ind]
-            cx, cy = self.values['pin_centers'][ind]
-            cx, cy = cx + pcb_cx, cy + pcb_cy
-            footprint.SetOrientationDegrees(np.rad2deg(-(angle - np.pi/2)))
-            pos = pcbnew.VECTOR2I(pcbnew.FromMM(float(cx)),pcbnew.FromMM(float(cy)))
+        # Get component data, e.g., positions and angles of all panel headers
+        # and relative components
+        cur_comp_data = get_cur_comp_data(pcb, panel_ref_list, panel_ref_to_rel)
+        new_comp_data = get_new_comp_data(self.values, pcb_params, panel_ref_list, 
+                panel_ref_to_rel, cur_comp_data)
+
+        for ref, data in new_comp_data.items():
+            footprint = pcb.FindFootprintByReference(ref)
+            xval = pcbnew.FromMM(data['x'])
+            yval = pcbnew.FromMM(data['y'])
+            pos = pcbnew.VECTOR2I(xval,yval)
             footprint.SetPosition(pos)
+            footprint.SetOrientationDegrees(np.rad2deg(data['angle']))
+
 
         pcb.Save('test.kicad_pcb')
             
-
-        
-
 
 
     def get_pcb_params(self):
@@ -227,6 +234,7 @@ class RingLayout:
         print(f'    ref_start:   {pcb_params["panel"]["ref_start"]}')
         print(f'  relative')   
         print(f'    model:       {pcb_params["relative"]["model"]}')
+        print()
 
 
     def load_config(self, config):
@@ -368,18 +376,173 @@ def get_pin_pos(angles, num, radius, pitch, omitted):
 
 
 
+def get_panel_ref_to_rel(pcb_params, panel_num_list, panel_ref_list): 
+    """ 
+    Get dictionary of relative components for placement with each panel header
+
+    """
+    # panel header
+    panel_ref_to_rel = {}
+    for panel_num, panel_ref in zip(panel_num_list, panel_ref_list):
+        try:
+            rel_comps = pcb_params['relative'][f'{panel_num}']
+        except KeyError:
+            print(f'relative comps for {panel_num} not found')
+        else:
+            panel_ref_to_rel[panel_ref] = rel_comps
+    return panel_ref_to_rel
 
 
+def get_comp_data_by_ref(pcb, ref): 
+    """
+    Gets the component data, x y position (mm) and angle (radians),
+    for the given component reference.
+
+    Parameters
+    ----------
+    pcb : Board
+          kicad pcbnew board
+
+    ref : string
+          component reference designator
+
+    Returns
+    -------
+    comp_data : dict
+                dictionary of componetn data x, y, angle
+
+    """
+    footprint = pcb.FindFootprintByReference(ref)
+    position = footprint.GetPosition()
+    angle = footprint.GetOrientation().AsRadians()
+    comp_data = {
+            'x'     : pcbnew.ToMM(position.x),
+            'y'     : pcbnew.ToMM(position.y),
+            'angle' : angle,
+            }
+    return comp_data
 
 
+def get_cur_comp_data(pcb, panel_ref_list, panel_ref_to_rel): 
+    """
+    Get the component data for all panel headers and relative components.
+
+    Parameters
+    ----------
+    pcb              : Board
+                       kicad pcbnew board 
+
+    panel_ref_list   : list
+                       list of panel header references
+
+    panel_ref_to_rel : dict
+                       dictionary mapping panel reference to list of
+                       relative component references.
+
+    Returns:
+    comp_data : dict
+                dictionary of mapping reference to component data
+                e.g. x,y positions and orientation angles
+    """
+    comp_data = {}
+    for panel_ref in panel_ref_list:
+        comp_data[panel_ref] = get_comp_data_by_ref(pcb, panel_ref)
+        for rel_comp_ref in panel_ref_to_rel[panel_ref]:
+            comp_data[rel_comp_ref] = get_comp_data_by_ref(pcb, rel_comp_ref)
+    return comp_data
 
 
+def get_new_comp_data(arena_values, pcb_params, panel_ref_list, panel_ref_to_rel, cur_comp_data): 
+    """
+    Get new component placement data (x,y coords and angle) given the arena
+    values, pcb parameters and model component in pcb file.
 
+    Parameters
+    ----------
+    arena_values     : dict
+                       dictionary of arena values, e.g. panel angle, x,y positions, etc. 
 
+    pcb_params       : dict
+                       dictionary of pcb layout parameters
 
+    panel_ref_list   : list
+                       list of panel header reference designators
 
+    panel_ref_to_rel : dict
+                       dictionary mapping panel references to list of relative component
+                       references.
 
+    cur_comp_data    : dict
+                       dictionary of mapping reference to component data as currently in 
+                       pcb file e.g. x,y positions and orientation angles
 
+    Returns
+    -------
+    new_comp_data    : dict
+                       dictionary of mapping reference to component data for desired layuot  
+                       e.g. x,y positions and orientation angles
+                    
+    """
 
+    # Extract info for calculating new positions and orientatins
+    pcb_cx = pcb_params['center_x']
+    pcb_cy = pcb_params['center_y']
+    ref_prefix = pcb_params['panel']['ref_prefix']
+    model_num = pcb_params['relative']['model']
+    model_ref = f'{ref_prefix}{model_num}'
+    angles = arena_values['angles']
+    pin_centers = arena_values['pin_centers']
+
+    # Get desired x,y positions and angles for panel headers
+    new_comp_data = {}
+    for ind, panel_ref in enumerate(panel_ref_list):
+        angle = -(angles[ind] - np.pi/2)
+        cx, cy = pin_centers[ind]
+        cx, cy = float(cx + pcb_cx), float(cy + pcb_cy)
+        new_comp_data[panel_ref] = {'x': cx, 'y': cy, 'angle': angle }
+
+    # Get model data and data for relative components
+    model_data = cur_comp_data[model_ref]
+    model_rel_data = [cur_comp_data[item] for item in panel_ref_to_rel[model_ref]]
+    
+    # Shift and rotate model and relative components so that model component center
+    # is at origin and rotation is 0.0
+    angle = model_data['angle']
+    rot_matrix = np.array([
+        [np.cos(-angle), -np.sin(-angle)],
+        [np.sin(-angle),  np.cos(-angle)],
+        ])
+    for data in model_rel_data:
+        p = np.array([data['x']-model_data['x'], data['y'] - model_data['y']])
+        p = np.dot(rot_matrix, p)
+        data['x'] = p[0]
+        data['y'] = p[1]
+        data['angle'] = data['angle'] - angle 
+    model_data['x'] = 0.0
+    model_data['y'] = 0.0
+    model_data['angle'] = 0.0
+
+    # Get placements for all relative components
+    for panel_ref, rel_comp_ref_list in panel_ref_to_rel.items():
+        cx = new_comp_data[panel_ref]['x']
+        cy = new_comp_data[panel_ref]['y']
+        angle = -new_comp_data[panel_ref]['angle']
+        rot_matrix = np.array([
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle),  np.cos(angle)],
+            ])
+        for ind, rel_comp_ref in enumerate(rel_comp_ref_list):
+            data = model_rel_data[ind]
+            p = np.array([data['x'], data['y']])
+            p = np.dot(rot_matrix, p)
+            rel_angle = data['angle']
+            new_comp_data[rel_comp_ref] = {
+                    'x'     :  float(cx + p[0]),
+                    'y'     :  float(cy + p[1]),
+                    'angle' :  -(angle-rel_angle),
+                    }
+    return new_comp_data
+
+            
 
 
